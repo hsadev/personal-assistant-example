2c7d7df78fd00ad0a50b813b13397d935f6645a9
Peter Bynum <pkpbynum@gmail.com>
2019-12-30 17:18:46 -0500

    Updates

---
+++ precache-manifest.b8fe10bf584cad6b417767cf08abb390.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c66c3ed3f62d1d97916ad0e1efed5b60",
+    "url": "/personal-assistant-example/index.html"
+  },
+  {
+    "revision": "c017a989d7eac9da93a5",
+    "url": "/personal-assistant-example/static/js/2.8ed8d27c.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/personal-assistant-example/static/js/2.8ed8d27c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a17725a512c5373603c9",
+    "url": "/personal-assistant-example/static/js/main.59352bef.chunk.js"
+  },
+  {
+    "revision": "67a8c17def03b9e3cdbd",
+    "url": "/personal-assistant-example/static/js/runtime-main.4786c52b.js"
+  }
+]);
